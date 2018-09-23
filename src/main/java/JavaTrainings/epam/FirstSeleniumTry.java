6ab81fd53ed72a128d39ac51b5489b41652965ba
marat-karamian <marat_karamian@epam.com>
2018-09-23 15:13:41 +0300

    Initial commit

---
+++ src/main/java/JavaTrainings/epam/FirstSeleniumTry.java
@@ -0,0 +1,22 @@
+package JavaTrainings.epam;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class FirstSeleniumTry {
+
+    public static void main(String[] args) throws Throwable {
+
+        String exepath = "C:\\Users\\mkara\\Documents\\JavaMP\\chromedriver.exe";
+        System.setProperty("webdriver.chrome.driver", exepath);
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://www.google.com/");
+
+        System.out.println("Successfully opened the website https://www.google.com/");
+
+        Thread.sleep(5000);
+
+        driver.quit();
+    }
+}
