ef146bf65a2dbb418ca4734551bcddd7afa2ccf3
marat-karamian <marat_karamian@epam.com>
2018-10-03 17:56:44 +0300

    Few classes added. Classes and Methods renamed.

---
+++ src/main/java/JavaTrainings/epam/ResizeWindow.java
@@ -0,0 +1,48 @@
+package JavaTrainings.epam;
+
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.Point;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.awt.*;
+
+public class ResizeWindow {
+
+    public void resize() throws InterruptedException {
+
+        DriverConfig driverConfig = new DriverConfig();
+        driverConfig.config();
+        WebDriver driver = new ChromeDriver();
+        Thread.sleep(1000);
+
+        System.out.println(driver.manage().window().getSize());
+
+        driver.manage().window().setSize(new Dimension(500, 600));
+        System.out.println(driver.manage().window().getSize());
+        Thread.sleep(1000);
+
+        System.out.println(driver.manage().window().getPosition());
+        driver.manage().window().setPosition(new Point(200,150));
+        System.out.println(driver.manage().window().getPosition());
+        Thread.sleep(1000);
+
+        driver.manage().window().maximize();
+        System.out.println(driver.manage().window().getSize());
+        Thread.sleep(1000);
+
+        driver.manage().window().setPosition(new Point(0,0));
+        driver.manage().window().setSize(new Dimension(500,600));
+        Thread.sleep(1000);
+
+        Toolkit toolkit = Toolkit.getDefaultToolkit();
+        int Width = toolkit.getScreenSize().width;
+        int Height = toolkit.getScreenSize().height;
+        Dimension screenResolution = new Dimension(Width, Height);
+        driver.manage().window().setSize(screenResolution);
+        System.out.println(driver.manage().window().getSize());
+        Thread.sleep(2000);
+
+        driver.quit();
+    }
+}
---
+++ src/main/java/JavaTrainings/epam/RespEarth.java
@@ -0,0 +1,21 @@
+package JavaTrainings.epam;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class RespEarth {
+
+    public void earth() throws InterruptedException {
+
+        DriverConfig driverConfig = new DriverConfig();
+        driverConfig.config();
+        WebDriver driver = new ChromeDriver();
+
+        String url = "http://resp-earth.ctco-ecom.projects.epam.com";
+        driver.get(url);
+
+        Thread.sleep(3000);
+
+        driver.quit();
+    }
+}
